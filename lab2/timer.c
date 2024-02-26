5b966dc5b37ca56a04b88ecd71b4fb36898e36fa
DiogoRamos9 <up202207954@up.pt>
2024-02-26 21:34:17 +0000

    Timer_get_Conf timer.c implementado

--- lab2/timer.c
+++ lab2/timer.c
@@ -36,15 +36,17 @@ void (timer_int_handler)() {
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if(st == NULL) return 1;
+  if(timer > 2) return 1;
+  uint8_t rb_cmd = BIT(7)|BIT(6)|BIT(4)|BIT(timer + 1);
+  int outf = sys_outb(0x43, rb_cmd);
+  if(outf != 0) return 1;
+  int inf = util_sys_inb(timer + TIMER_0, st);
+  if(inf != 0) return 1;
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
+int (timer_display_conf)(uint8_t timer, uint8_t st,enum timer_status_field field) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
   return 1;
 }
