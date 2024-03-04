32e4706d4337ac65f294e3f6fb9e28f21053c18e
Yussee-1 <up202207784@up.pt>
2024-03-04 16:39:45 +0000

    utils.c done

---
+++ lab2/utils.c
@@ -0,0 +1,23 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL) return 1;
+  *lsb = val & 0xFF ;
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  if (msb == NULL) return 1;
+  *msb = (val >> 8) & 0xFF;
+  return 1;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  if (value == NULL) return 1;
+  uint32_t value_32;
+  int v = sys_inb(port,&value_32);
+  *value = 0xFF & value32;
+  return v;
+}
