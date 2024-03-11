1d0339bb1481e97c193ec9130b8fb39cce73ed1a
Yussee-1 <up202207784@up.pt>
2024-03-11 15:52:29 +0000

    Timer.c and Lab2.c almost fully complete.

--- lab2/timer.c
+++ lab2/timer.c
@@ -4,31 +4,42 @@
 #include <stdint.h>
 
 #include "i8254.h"
+int timer_hook_id = 0;
+int counter;
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (freq > TIMER_FREQ || freq < 19) return 1;
+  
+  uint8_t CTRL_WRD;
+  if (timer_get_conf(timer,&CTRL_WRD)) return 1;
 
-  return 1;
+  CTRL_WRD |= TIMER_LSB_MSB | (timer << 6);
+
+  uint32_t TIME;
+  TIME = TIMER_FREQ / freq;
+  uint8_t TIME_LSB; uint8_t TIME_MSB;
+  if (util_get_LSB(TIME,&TIME_LSB)) return 1;
+  if (util_get_MSB(TIME,&TIME_MSB)) return 1;
+
+  if (sys_outb(TIMER_CTRL,CTRL_WRD)) return 1;
+  if (sys_outb(TIMER_0 + timer,TIME_LSB)) return 1;
+  if (sys_outb(TIMER_0 + timer,TIME_MSB)) return 1;
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
-}
+if (bit_no == NULL) return 1;
+  *bit_no = BIT(timer_hook_id);
+  return sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &timer_hook_id);
+  }
 
 int (timer_unsubscribe_int)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return sys_irqrmpolicy(&timer_hook_id); 
 }
 
 void (timer_int_handler)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  counter++;
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
@@ -36,16 +47,38 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
   uint8_t RB_CODE = BIT(7) | BIT(6) | BIT(5) | BIT(timer + 1);
 
-  if(util_sys_outb(0x43,RB_CODE) == 1) return 1;
-  int v = util_sys_inb(0x40 + timer,*st);
+  if(sys_outb(0x43,RB_CODE) == 1) return 1;
+  int v = util_sys_inb(0x40 + timer,st);
 
   return v;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  union timer_status_field_val data;  
 
-  return 1;
+  switch (field) {
+    case tsf_all:
+      data.byte = st;
+    case tsf_initial:
+      st >>= 4;
+      st &= (BIT(0) | BIT(1));
+      if (!st) data.in_mode = INVAL_val;
+      if (st == 1) data.in_mode = LSB_only;
+      if (st == 2) data.in_mode = MSB_only;
+      if (st == 3) data.in_mode = MSB_after_LSB;
+    case tsf_mode:
+      st >>= 1;
+      st &= (BIT(0) | BIT(1) | BIT(2));
+      data.count_mode = st;
+    case tsf_base:  
+      st &= BIT(1);
+      data.bcd = st;
+    default:
+      return 1;
+  }
+
+  if(timer_print_config(timer,field,data)) return 1;
+
+  return 0;
 }
